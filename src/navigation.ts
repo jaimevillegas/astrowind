4e298ca413b80df795db32ebeaa3e55d19f79638
Jaime Villegas <jaimevillegas296@gmail.com>
2024-12-06 18:03:40 -0500

    Fix navigation to contact form

--- src/navigation.ts
+++ src/navigation.ts
@@ -25,47 +25,20 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Navigation',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Home', href: '/' },
+        { text: 'Projects', href: '/projects' },
+        { text: 'Blog', href: '/blog' },
+        { text: 'Contact', href: '/contact' },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Latest Posts',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'React App Optimization', href: '/blog/react-app-optimization-better-content-loading' },
+        { text: 'Using Vim in VSCode', href: '/blog/using-vim-in-vscode-improve-coding' },
+        { text: 'TailwindCSS Animated Loader', href: '/blog/using-tailwindcss-to-create-animated-loader' },
       ],
     },
   ],
@@ -74,13 +47,11 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/jaimevillegas/' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/jaimevillegas' },
+    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:jaimevillegas296@gmail.com' },
   ],
   footNote: `
-  Copyright © 2024
+    Copyright ${new Date().getFullYear()} Jaime Villegas. All rights reserved.
   `,
 };
